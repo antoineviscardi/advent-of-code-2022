1d40ac48b72609bec851b01c58f253e2cc7f357a
Antoine Viscardi <me@antoineviscardi.com>
2022-12-15 20:16:52 -0500

    wip day7

--- src/challenges/day7/filesystem.rs
+++ src/challenges/day7/filesystem.rs
@@ -1,26 +1,55 @@
-struct Filesystem {
+pub struct Filesystem {
     root: Directory,
 }
 
-struct Directory {
+impl Filesystem {
+    pub fn init() -> Self {
+        Filesystem {
+            root: Directory::new("/"),
+        }
+    }
+}
+
+#[cfg(test)]
+mod filesystem_tests {
+    use super::*;
+
+    #[test]
+    fn test_init_works() {
+        let fs = Filesystem::init();
+        assert_eq!(fs.root.name, "/");
+        assert!(fs.root.files.is_empty());
+    }
+}
+
+pub struct Directory {
     name: String,
     files: Vec<File>,
+}
+
+impl Directory {
+    pub fn new(name: &str) -> Self {
+        Directory {
+            name: name.to_string(),
+            files: Vec::new(),
+        }
+    }
+}
+
+#[cfg(test)]
+mod directory_test {
+    use super::*;
+
+    #[test]
+    fn test_new_works() {
+        let name = "cool dir bro";
+        let dir = Directory::new(name);
+        assert_eq!(dir.name, name.to_string());
+        assert!(dir.files.is_empty());
+    }
 }
 
 enum File {
     Directory,
     Document,
-}
-
-impl Filesystem {
-    /// Factory function that initializes a Filesystem with an empty root Directory.
-    ///
-    /// ```
-    /// let filesystem = Filesystem::init();
-    /// assert_eq!(filesystem.root.name, "/");
-    /// assert_eq(filesystem.root.files.is_empty());
-    ///
-    pub fn init() -> Self {
-        todo!()
-    }
 }
